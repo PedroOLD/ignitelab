216dce38d6f750fa204bd8129ff1369de884c7bc
Pedro Henrique Dos Santos <pedro.dossantos@meta.com.br>
2022-06-23 08:22:27 -0300

    atualizações

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,11 @@
-import { gql, useQuery } from "@apollo/client"
-
-const GET_LESSSONS_QUERY = gql`
-    query {
-      lessons{
-        id
-        title
-      }
-    }
-
-  `
-
-  interface Leassons {
-    id: string
-    title: string
-  }
+import { Event } from "./pages/Event";
 
 function App() {
 
-
-  const { data } = useQuery<{lessons: Leassons[]}>(GET_LESSSONS_QUERY);
-  console.log(data)
-
   return (
-    <ul>
-        {data?.lessons.map((lesson) => {
-            return <li key={lesson.id}>{lesson.title}</li>
-        })}
-    </ul>
+    <div>
+      <Event />
+    </div>
   )
 }
 
